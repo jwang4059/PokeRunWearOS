8dd5ccb5684874a5f30d45038faeb64f855bd802
jwang0110 <johnw7@uci.edu>
2023-05-16 16:46:47 -0700

    add hilt and dagger

--- app/src/main/java/com/example/pokerunwearos/data/ExcerciseClientManager.kt
+++ app/src/main/java/com/example/pokerunwearos/data/ExcerciseClientManager.kt
@@ -3,10 +3,13 @@ package com.example.pokerunwearos.data
 import androidx.health.services.client.HealthServicesClient
 import androidx.health.services.client.data.ExerciseType
 import androidx.health.services.client.data.ExerciseTypeCapabilities
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.guava.await
+import javax.inject.Inject
 
-class ExerciseClientManager (
+class ExerciseClientManager @Inject constructor (
     healthServicesClient: HealthServicesClient,
+    coroutineScope: CoroutineScope
 ) {
     private val exerciseClient = healthServicesClient.exerciseClient
     private var exerciseCapabilities: ExerciseTypeCapabilities? = null
